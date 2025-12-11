2bc8434730f603f35c44c0585682580a7605416f
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-12-11 23:08:11 +0530

    initial commit

---
+++ megablog/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="w-full h-auto">
+      <div className="flex bg-gray-300 rounded-lg border-2">
+        <div>LOGO</div>
+        <div>
+          <nav className=""></nav>
+        </div>
+      </div>
+    </div>
+  );
+}
