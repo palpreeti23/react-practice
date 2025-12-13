0d73050093c35ca299d815c972f7466a846809d8
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-12-13 22:02:37 +0530

    updated

--- megablog/src/components/index.js
+++ megablog/src/components/index.js
@@ -6,5 +6,20 @@ import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import LogoutBtn from "./header/LogoutBtn";
 import Select from "./Select";
+import Login from "./Login";
+import Signup from "./Signup";
+import RTE from "./RTE";
 
-export { Container, Button, Input, Logo, Header, Footer, LogoutBtn, Select };
+export {
+  Container,
+  Button,
+  Input,
+  Logo,
+  Header,
+  Footer,
+  LogoutBtn,
+  Select,
+  Login,
+  Signup,
+  RTE,
+};
