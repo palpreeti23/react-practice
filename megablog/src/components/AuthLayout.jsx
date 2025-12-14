ed70bcc3d50b72111a896fa2d0eda43d7692a099
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-12-14 22:38:42 +0530

    authlayout done

---
+++ megablog/src/components/AuthLayout.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function AuthLayout({ children, authentication = true }) {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    if (authentication && authStatus != authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus != authentication) {
+      navigate("/");
+    }
+    setLoading(false);
+  }, [navigate, authStatus, authentication]);
+
+  return loading ? <p>loading...</p> : <div>{children}</div>;
+}
+
+export default AuthLayout;
