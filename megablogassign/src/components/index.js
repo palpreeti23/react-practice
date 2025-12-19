7da3a9dc7403b7e2c84207b33d9adae8d3d52151
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-12-19 21:32:10 +0530

    chore: add index.js for centralized imports and exports

---
+++ megablogassign/src/components/index.js
@@ -0,0 +1,25 @@
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import LogoutBtn from "../components/header/LogoutBtn";
+import Container from "./Container";
+import Input from "./Input";
+import Button from "./Button";
+import Logo from "./Logo";
+import RTE from "./RTE";
+import Select from "./Select";
+import Login from "./Login";
+import Signup from "./Signup";
+
+export {
+  Header,
+  Footer,
+  LogoutBtn,
+  Container,
+  Input,
+  Button,
+  Logo,
+  RTE,
+  Select,
+  Login,
+  Signup,
+};
