056273b2da3f8ec158a077f8db3b919d0ad8f675
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-12-19 22:44:16 +0530

    postCard done

---
+++ megablogassign/src/components/PostCard.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import appwriteService from "../appwrite/conf";
+import { useDispatch } from "react-redux";
+
+function PostCard({ $id, featuredImage, title }) {
+  const images = featuredImage
+    ? appwriteService.getFilePreview(featuredImage)
+    : null;
+
+  return (
+    <Link to={`/post/${$id}`}>
+      <div className="w-full">
+        <div className="flex flex-col justify-items-center">
+          <img
+            className="w-full h-30 object-contain rounded-lg"
+            src={images}
+            alt="content Image"
+          />
+          <h2 className="text-black font-bold text-xl text-center">{title}</h2>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default PostCard;
